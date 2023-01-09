2361f826a52626c45481257fadc8e1d688f3b332
PouyaJigsaw <pouya__1__pournasir@hotmail.com>
2023-01-09 19:00:01 -0400

    server_in_view

--- src/test/src/main/view/server.py
+++ src/test/src/main/view/server.py
@@ -1,15 +1,20 @@
 import socket 
-
+import socketserver
 
 HOST = '192.168.2.15'
 PORT = 4001
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST,PORT))
-s.listen()
-conn, addr = s.accept()
-while True:
-    data = conn.recv(1024)
-    data = data.decode('utf-8')
-    print("Message from: " + str(addr))
-    print("From connected user: " + data)
+socketserver.TCPServer.allow_reuse_address = True
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            data = data.decode('utf-8')
+            if not data:
+                break
+            
