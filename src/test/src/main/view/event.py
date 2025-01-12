e786978f1d9e84fc25eb1f1b5ed7a2522f8f4f70
leno-aph508 <aresparvizi@gmail.com>
2025-01-12 19:20:13 -0400

    Add event manager

--- src/test/src/main/view/event.py
+++ src/test/src/main/view/event.py
@@ -1,19 +1,34 @@
 from collections import defaultdict
 from sensor_msgs.msg import CompressedImage
+from event_manager import EventManager
+
+EVENT_FREEZE = "freeze"
+EVENT_UNFREEZE = "unfreeze"
+EVENT_CALIBRATE = "calibrate"
+EVENT_CALIBRATE_PAUSE = "calibrate_pause"
+EVENT_JOY = "joy"
+EVENT_COLLISION = "collision"
+EVENT_CONGRATULATIONS = "congratulations"
+
+# Register all events
+EventManager.register_event(EVENT_FREEZE)
+EventManager.register_event(EVENT_UNFREEZE)
+EventManager.register_event(EVENT_CALIBRATE)
+EventManager.register_event(EVENT_CALIBRATE_PAUSE)
 
 
-class EventManager():
+# class EventManager():
 
-    _subscribers = defaultdict(list)
+#     _subscribers = defaultdict(list)
 
-    def subscribe(event_type, fn):
-        EventManager._subscribers[event_type].append(fn)
+#     def subscribe(event_type, fn):
+#         EventManager._subscribers[event_type].append(fn)
 
-    def post_event(event_type, data=None): 
+#     def post_event(event_type, data=None): 
         
-        if event_type in EventManager._subscribers:
-            for fn in EventManager._subscribers[event_type]:
-                if data is None:
-                    fn(event_type)
-                else:
-                    fn(data)
+#         if event_type in EventManager._subscribers:
+#             for fn in EventManager._subscribers[event_type]:
+#                 if data is None:
+#                     fn(event_type)
+#                 else:
+#                     fn(data)
